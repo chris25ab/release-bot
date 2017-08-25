5b98f03430f0c10b0f4a6a6faea58f2916fe9272
Christian <cabarca250793@gmail.com>
2017-08-25 09:24:25 -0600

    Change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, 'metricbot'
-set :repo_url, 'https://github.com/briverac/metricbot.git'
+set :application, 'release-bot'
+set :repo_url, 'https://github.com/chris25ab/release-bot.git'
 
 set :rvm_ruby_version, (ENV['RVM_RUBY_STRING'] || 'ruby-2.2.6')
 
@@ -15,12 +15,9 @@ branch = 'develop' if branch.nil?
 
 set :branch, branch
 
-set :deploy_to, '/www/metricbot'
+set :deploy_to, '/www/release-bot'
 
 set :ssh_options, { forward_agent: true }
-
-# Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/credentials.yml')
 
 namespace :deploy do
 end
